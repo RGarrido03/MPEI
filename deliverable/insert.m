62fec3bd1548b601c9fb59a50652db15f7c60f03
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2023-12-21 11:40:49 +0000

    Clean unused code

--- deliverable/insert.m
+++ deliverable/insert.m
@@ -144,28 +144,3 @@ for n = 1:Nm
     end
 end
 end
-
-%function SimilarTitles = createMovieSimilarities(Set_title, Nm, signatures_title)
-%    J = zeros(Nm);
-%    k = 100;
-%    for n1= 1:Nm
-%        for n2= n1+1:Nm
-%            J(n1, n2) = sum(signatures_title(n1,:) ~= signatures_title(n2,:))/k;
-%        end
-%    end
-%    
-%    
-%    % Array para guardar títulos similares (título1, título2, similaridade)
-%    SimilarTitles= zeros(1,3);
-%    k= 1;
-%    for n1= 1:Nm
-%        for n2= n1+1:Nm
-%            SimilarTitles(k,:) = [Set_title(1) Set_title(2) 1-J(n1,n2)];
-%            k= k+1;
-%        end
-%    end
-%
-%    if ~all(SimilarTitles(:) == 0)
-%        SimilarTitles = sortrows(SimilarTitles, 3);
-%    end
-%end
